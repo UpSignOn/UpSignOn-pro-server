3cc39872f2b191907bb79656c6cdf4e8ef3e9eae
Gireg <gireg.dekerdanet@hotmail.fr>
2021-10-15 21:41:39 +0200

    enable cors on config route

--- src/routes/getConfig.ts
+++ src/routes/getConfig.ts
@@ -2,6 +2,7 @@ import env from '../helpers/env';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 export const getConfig = async (req: any, res: any): Promise<void> => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   return res.status(200).json({
     displayName: env.ORGANISATION_NAME,
   });
