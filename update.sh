0338f6d77daf63c57661c11d881e7509a889560d
Gireg <gireg.dekerdanet@hotmail.fr>
2022-03-01 15:08:03 +0100

    make update script more resilient

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 git pull origin production --ff-only
 yarn install
-yarn build
+rm -rf compiled
+tsc -p .
 node ./scripts/migrateUp.js
-yarn restart
+pm2 del upsignon-pro-server
+pm2 startOrReload ecosystem.config.js --only upsignon-pro-server
