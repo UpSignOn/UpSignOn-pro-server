949587cf6cafdb245253b2898c78351eab28419d
Gireg <gireg.dekerdanet@hotmail.fr>
2022-03-10 23:45:40 +0100

    fix update script

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 git pull origin production --ff-only
 yarn install
-rm -rf compiled
-tsc -p .
+yarn build
 node ./scripts/migrateUp.js
-pm2 del upsignon-pro-server
 pm2 startOrReload ecosystem.config.js --only upsignon-pro-server
