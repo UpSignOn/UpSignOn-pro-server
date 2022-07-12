fc714a978ad85a48cdec6092bf5ba500dd5f415c
Gireg <gireg.dekerdanet@hotmail.fr>
2022-07-12 20:09:18 +0200

    fix email regex

--- src/routes/requestAccess.ts
+++ src/routes/requestAccess.ts
@@ -26,7 +26,7 @@ export const requestAccess = async (req: any, res: any) => {
 
     // Get params
     let userEmail = req.body?.userEmail;
-    const emailRegex = RegExp('^([a-zA-Z0-9_-.]+)@([a-zA-Z0-9_-.]+).([a-zA-Z]{2,5})$', 'g');
+    const emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+).([a-zA-Z]{2,5})$/g;
     if (!userEmail || typeof userEmail !== 'string' || !emailRegex.test(userEmail))
       return res.status(401).end();
     userEmail = userEmail.toLowerCase();
