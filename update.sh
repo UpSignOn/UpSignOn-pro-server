abd7667c267d343b7d477c39063b13ea66728359
Gireg <gireg.dekerdanet@hotmail.fr>
2022-09-23 12:42:12 +0200

    prevent the update script to be run as another user than upsignonpro

--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+if [[ "$USER" != "upsignonpro" && "$USER" != "upsignon" ]]; then
+  echo "You need to run the update script as upsignonpro."
+  exit 1
+fi
+
 git pull origin production --ff-only
 yarn install
 yarn build
