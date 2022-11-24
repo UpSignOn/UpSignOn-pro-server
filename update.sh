8e4bbdbd27bcf8ff09b4f5b42de69cae10bfe018
Gireg <gireg.dekerdanet@hotmail.fr>
2022-11-24 19:17:21 +0100

    upgrade dependencies and fix update script

--- update.sh
+++ update.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-if [[ "$USER" != "upsignonpro" && "$USER" != "upsignon" ]]; then
-  echo "You need to run the update script as upsignonpro."
-  exit 1
-fi
-
 git pull origin production --ff-only
 yarn install
 yarn build
 node ./scripts/migrateUp.js
-pm2 startOrReload ecosystem.config.js --only upsignon-pro-server
+pm2 stop upsignon-pro-server
+pm2 start ecosystem.config.js
