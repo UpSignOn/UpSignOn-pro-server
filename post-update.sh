43d1a1eca82c9f59c2c2f0906a929a0966ad6bd2
Gireg <gireg.dekerdanet@hotmail.fr>
2023-03-10 11:59:08 +0100

    fix update script

--- post-update.sh
+++ post-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-/home/upsignonpro/.npm-global/bin/yarn install
-/home/upsignonpro/.npm-global/bin/yarn build
+~/.npm-global/bin/yarn install
+~/.npm-global/bin/yarn build
 /usr/bin/node ./scripts/migrateUp.js
-/home/upsignonpro/.npm-global/bin/pm2 stop upsignon-pro-server
-/home/upsignonpro/.npm-global/bin/pm2 start ecosystem.config.js --update-env
+~/.npm-global/bin/pm2 stop upsignon-pro-server
+~/.npm-global/bin/pm2 start ecosystem.config.js --update-env
