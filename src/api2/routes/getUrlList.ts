eb97d4848e409d192f27df25de22616cfbdfd862
Gireg <giregk@upsignon.eu>
2023-05-18 23:50:50 +0200

    start an api v2

---
+++ src/api2/routes/getUrlList.ts
@@ -0,0 +1,27 @@
+import { checkBasicAuth } from '../../helpers/authorizationChecks';
+import { db } from '../../helpers/db';
+import { logError } from '../../helpers/logger';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
+export const getUrlList = async (req: any, res: any): Promise<void> => {
+  try {
+    const basicAuth = await checkBasicAuth(req);
+    if (!basicAuth.granted) return res.status(401).end();
+
+    const urlListRes = await db.query(
+      'SELECT * FROM url_list WHERE group_id=$1 ORDER BY displayed_name ASC',
+      [basicAuth.groupId],
+    );
+
+    const list = urlListRes.rows.filter(u => !!u.displayed_name).map(u => ({
+      name: u.displayed_name,
+      url: u.signin_url
+    }))
+
+    // Return res
+    return res.status(200).json({ urlList: list });
+  } catch (e) {
+    logError('getUrlList', e);
+    return res.status(400).end();
+  }
+};
