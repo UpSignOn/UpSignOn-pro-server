24041dc07b91dfac5d9d93d5438de093e45eff31
Gireg <giregk@upsignon.eu>
2023-05-30 18:12:33 +0200

    add vault and shared vault stats into update vault queries

--- src/api2/routes/data/updateVaultData.ts
+++ src/api2/routes/data/updateVaultData.ts
@@ -9,10 +9,12 @@ export const updateVaultData = async (req: any, res: any): Promise<void> => {
   try {
     const newEncryptedData = inputSanitizer.getString(req.body?.newEncryptedData);
     const lastUpdatedAt = inputSanitizer.getString(req.body?.lastUpdatedAt);
+    const vaultStats = inputSanitizer.getVaultStats(req.body?.vaultStats);
 
     // Check params
     if (!newEncryptedData) return res.status(403).end();
     if (!lastUpdatedAt) return res.status(403).end();
+    if (!vaultStats) return res.status(403).end();
 
     const basicAuth = await checkBasicAuth2(req, {});
     if (!basicAuth.granted) return res.status(401).end();
@@ -20,12 +22,45 @@ export const updateVaultData = async (req: any, res: any): Promise<void> => {
     const newEncryptedDataWithPasswordChallengeSecured =
       hashPasswordChallengeResultForSecureStorage(newEncryptedData);
     const updateRes = await db.query(
-      'UPDATE users SET (encrypted_data, updated_at)=($1, CURRENT_TIMESTAMP(0)) WHERE users.email=$2 AND users.updated_at=CAST($3 AS TIMESTAMPTZ) AND users.group_id=$4 RETURNING updated_at',
+      `UPDATE users
+        SET (
+          encrypted_data,
+          updated_at,
+          nb_accounts,
+          nb_codes,
+          nb_accounts_strong,
+          nb_accounts_medium,
+          nb_accounts_weak,
+          nb_accounts_with_duplicated_password,
+          nb_accounts_with_no_password,
+          nb_accounts_red,
+          nb_accounts_orange,
+          nb_accounts_green
+          )=(
+            $1,
+            CURRENT_TIMESTAMP(0),
+            $5,$6,$7,$8,$9,$10,$11,$12,$13,$14
+          )
+        WHERE
+          users.email=$2
+          AND users.updated_at=CAST($3 AS TIMESTAMPTZ)
+          AND users.group_id=$4
+        RETURNING updated_at`,
       [
         newEncryptedDataWithPasswordChallengeSecured,
         basicAuth.userEmail,
         lastUpdatedAt,
         basicAuth.groupId,
+        vaultStats.nbAccounts,
+        vaultStats.nbCodes,
+        vaultStats.nbAccountsStrong,
+        vaultStats.nbAccountsMedium,
+        vaultStats.nbAccountsWeak,
+        vaultStats.nbAccountsWithDuplicatedPassword,
+        vaultStats.nbAccountsWithNoPassword,
+        vaultStats.nbAccountsRed,
+        vaultStats.nbAccountsOrange,
+        vaultStats.nbAccountsGreen,
       ],
     );
     if (updateRes.rowCount === 0) {
