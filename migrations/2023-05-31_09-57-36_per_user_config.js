7a8e9076dd3938c8336d169f2e4847d0771d0c4f
Gireg <giregk@upsignon.eu>
2023-05-31 10:02:46 +0200

    add route user settings

---
+++ migrations/2023-05-31_09-57-36_per_user_config.js
@@ -0,0 +1,12 @@
+//2023-05-31_09-57-36_per_user_config
+
+exports.up = function (db) {
+  return db.query(`ALTER TABLE users
+  ADD COLUMN IF NOT EXISTS allowed_to_export BOOLEAN DEFAULT 0,
+  ADD COLUMN IF NOT EXISTS allowed_offline BOOLEAN DEFAULT 0
+`);
+}
+
+exports.down = function (db) {
+  return db.query();
+}
