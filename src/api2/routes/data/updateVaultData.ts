7ba3aa772ee55a20d9001a2c712997eb9bb21be3
Gireg <giregk@upsignon.eu>
2023-07-07 22:55:08 +0200

    feat: make it easier to migrate

--- src/api2/routes/data/updateVaultData.ts
+++ src/api2/routes/data/updateVaultData.ts
@@ -9,20 +9,21 @@ export const updateVaultData = async (req: any, res: any): Promise<void> => {
   try {
     const newEncryptedData = inputSanitizer.getString(req.body?.newEncryptedData);
     const lastUpdatedAt = inputSanitizer.getString(req.body?.lastUpdatedAt);
-    const vaultStats = inputSanitizer.getVaultStats(req.body?.vaultStats);
 
     // Check params
     if (!newEncryptedData) return res.status(403).end();
     if (!lastUpdatedAt) return res.status(403).end();
-    if (!vaultStats) return res.status(403).end();
 
     const basicAuth = await checkBasicAuth2(req, {});
     if (!basicAuth.granted) return res.status(401).end();
 
     const newEncryptedDataWithPasswordChallengeSecured =
       hashPasswordChallengeResultForSecureStorageV2(newEncryptedData);
-    const updateRes = await db.query(
-      `UPDATE users
+
+    if (req.body?.vaultStats) {
+      const vaultStats = inputSanitizer.getVaultStats(req.body?.vaultStats);
+      const updateRes = await db.query(
+        `UPDATE users
         SET (
           encrypted_data,
           updated_at,
@@ -40,35 +41,58 @@ export const updateVaultData = async (req: any, res: any): Promise<void> => {
             $1,
             CURRENT_TIMESTAMP(0),
             $5,$6,$7,$8,$9,$10,$11,$12,$13,$14
-          )
-        WHERE
-          users.email=$2
-          AND users.updated_at=CAST($3 AS TIMESTAMPTZ)
-          AND users.group_id=$4
-        RETURNING updated_at`,
-      [
-        newEncryptedDataWithPasswordChallengeSecured,
-        basicAuth.userEmail,
-        lastUpdatedAt,
-        basicAuth.groupId,
-        vaultStats.nbAccounts,
-        vaultStats.nbCodes,
-        vaultStats.nbAccountsStrong,
-        vaultStats.nbAccountsMedium,
-        vaultStats.nbAccountsWeak,
-        vaultStats.nbAccountsWithDuplicatedPassword,
-        vaultStats.nbAccountsWithNoPassword,
-        vaultStats.nbAccountsRed,
-        vaultStats.nbAccountsOrange,
-        vaultStats.nbAccountsGreen,
-      ],
-    );
-    if (updateRes.rowCount === 0) {
-      // CONFLICT
-      return res.status(409).json({ error: "outdated" });
-    }
+            )
+            WHERE
+            users.email=$2
+            AND users.updated_at=CAST($3 AS TIMESTAMPTZ)
+            AND users.group_id=$4
+            RETURNING updated_at`,
+        [
+          newEncryptedDataWithPasswordChallengeSecured,
+          basicAuth.userEmail,
+          lastUpdatedAt,
+          basicAuth.groupId,
+          vaultStats!.nbAccounts,
+          vaultStats!.nbCodes,
+          vaultStats!.nbAccountsStrong,
+          vaultStats!.nbAccountsMedium,
+          vaultStats!.nbAccountsWeak,
+          vaultStats!.nbAccountsWithDuplicatedPassword,
+          vaultStats!.nbAccountsWithNoPassword,
+          vaultStats!.nbAccountsRed,
+          vaultStats!.nbAccountsOrange,
+          vaultStats!.nbAccountsGreen,
+        ],
+      );
+      if (updateRes.rowCount === 0) {
+        // CONFLICT
+        return res.status(409).json({ error: 'outdated' });
+      }
 
-    return res.status(200).json({ lastUpdatedAt: updateRes.rows[0].updated_at });
+      return res.status(200).json({ lastUpdatedAt: updateRes.rows[0].updated_at });
+    } else {
+      const updateRes = await db.query(
+        `UPDATE users
+          SET (encrypted_data, updated_at)=($1,CURRENT_TIMESTAMP(0))
+          WHERE
+            users.email=$2
+            AND users.updated_at=CAST($3 AS TIMESTAMPTZ)
+            AND users.group_id=$4
+            RETURNING updated_at`,
+        [
+          newEncryptedDataWithPasswordChallengeSecured,
+          basicAuth.userEmail,
+          lastUpdatedAt,
+          basicAuth.groupId,
+        ],
+      );
+      if (updateRes.rowCount === 0) {
+        // CONFLICT
+        return res.status(409).json({ error: 'outdated' });
+      }
+
+      return res.status(200).json({ lastUpdatedAt: updateRes.rows[0].updated_at });
+    }
   } catch (e) {
     logError('updateData2', e);
     return res.status(400).end();
