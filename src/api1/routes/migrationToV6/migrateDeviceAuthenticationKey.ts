f066879a64a2044d98c5b17d307abfc51851753b
Gireg <giregk@upsignon.eu>
2023-07-11 15:33:33 +0200

    fix migrate device auth key

--- src/api1/routes/migrationToV6/migrateDeviceAuthenticationKey.ts
+++ src/api1/routes/migrationToV6/migrateDeviceAuthenticationKey.ts
@@ -6,7 +6,7 @@ import { checkBasicAuth } from '../../helpers/authorizationChecks';
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 export const migrateDeviceAuthenticationKey = async (req: any, res: any) => {
   try {
-    const basicAuth = await checkBasicAuth(req);
+    const basicAuth = await checkBasicAuth(req, {returningDeviceId: true});
     if (!basicAuth.granted) return res.status(401).end();
 
     const newDeviceAuthenticationKey = inputSanitizer.getString(req.body?.newDeviceAuthenticationKey);
@@ -14,10 +14,14 @@ export const migrateDeviceAuthenticationKey = async (req: any, res: any) => {
       return res.status(403).end();
     }
 
-    await db.query(
+    const updateRes = await db.query(
       'UPDATE user_devices SET device_public_key=$1 WHERE id=$2 AND user_id=$3 AND group_id=$4',
       [newDeviceAuthenticationKey, basicAuth.deviceId, basicAuth.userId, basicAuth.groupId]
     );
+    if(updateRes.rowCount != 1){
+      console.error("migrateDeviceAuthenticationKey updated "+updateRes.rowCount + "instead of 1");
+      return res.status(403).end();
+    }
     return res.status(204).end();
   } catch (e) {
     logError('migrateDeviceAuthenticationKey', e);
