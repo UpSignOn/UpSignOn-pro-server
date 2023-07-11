6c8bf922210a4c9ead51667f29979ff5824a0799
Gireg <giregk@upsignon.eu>
2023-07-11 13:17:09 +0200

    apply rename to sql requests

--- src/api2/routes/sharedVaults/updateSharedVaultData.ts
+++ src/api2/routes/sharedVaults/updateSharedVaultData.ts
@@ -76,7 +76,7 @@ export const updateSharedVaultData = async (req: any, res: any): Promise<void> =
         [authRes.userId, authRes.groupId],
       );
       await db.query(
-        'INSERT INTO data_stats (shared_vault_id, nb_accounts, nb_codes, nb_accounts_strong, nb_accounts_medium, nb_accounts_weak, nb_accounts_with_no_password, nb_accounts_with_duplicate_password, nb_accounts_red, nb_accounts_orange, nb_accounts_green, group_id) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)',
+        'INSERT INTO data_stats (shared_vault_id, nb_accounts, nb_codes, nb_accounts_strong, nb_accounts_medium, nb_accounts_weak, nb_accounts_with_no_password, nb_accounts_with_duplicated_password, nb_accounts_red, nb_accounts_orange, nb_accounts_green, group_id) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)',
         [
           sharedVaultId,
           vaultStats!.nbAccounts,
