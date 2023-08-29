2c9c6f99b0f6a8b4ef3ab70884187480bb4286bf
Gireg <giregk@upsignon.eu>
2023-08-29 17:49:04 +0200

    fix device appVersion during migration to v6

--- src/api1/routes/migrationToV6/getDevicesBlockingDataMigration.ts
+++ src/api1/routes/migrationToV6/getDevicesBlockingDataMigration.ts
@@ -1,6 +1,7 @@
 import { isStrictlyLowerVersion } from '../../../helpers/appVersionChecker';
 import { db } from '../../../helpers/db';
 import { logError } from '../../../helpers/logger';
+import { inputSanitizer } from '../../../helpers/sanitizer';
 import { checkBasicAuth } from '../../helpers/authorizationChecks';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
@@ -8,6 +9,14 @@ export const getDevicesBlockingDataMigration = async (req: any, res: any) => {
   try {
     const basicAuth = await checkBasicAuth(req);
     if (!basicAuth.granted) return res.status(401).end();
+
+    // update app version so that this device is no longer blocking the migration
+    await db.query("UPDATE user_devices SET app_version=$1 WHERE user_id=$2 AND group_id=$3 AND device_unique_id=$4", [
+      inputSanitizer.getString(req.body?.appVersion),
+      basicAuth.userId,
+      basicAuth.groupId,
+      basicAuth.deviceUId
+    ]);
 
     const outdatedDevices = await db.query(
       'SELECT id, device_name, device_type, app_version FROM user_devices WHERE user_id=$1 AND group_id=$2 AND authorization_status = \'AUTHORIZED\' AND device_unique_id!=$3',
