ef33dd35cd0e8d40aae2a40169e3cfd3bec01894
Gireg <giregk@upsignon.eu>
2023-10-10 13:14:54 +0200

    fix migrations

--- migrations/2023-10-10_11-17-21_split_sharing_pub_key.js
+++ migrations/2023-10-10_11-17-21_split_sharing_pub_key.js
@@ -1,10 +1,10 @@
 //2023-10-10_11-17-21_split_sharing_pub_key
 
 exports.up = async function (db) {
-  await db.query('ALTER TABLE users ADD COLUMN sharing_public_key_2 TEXT');
+  await db.query('ALTER TABLE users ADD COLUMN IF NOT EXISTS sharing_public_key_2 TEXT');
   await db.query('UPDATE users SET sharing_public_key_2 = sharing_public_key');
 };
 
 exports.down = async function (db) {
-  await db.query('ALTER TABLE users DROP COLUMN sharing_public_key_2');
+  await db.query('ALTER TABLE users DROP COLUMN IF EXISTS sharing_public_key_2');
 };
--- migrations/2023-10-10_13-04-57_split_encrypted_password_backup.js
+++ migrations/2023-10-10_13-04-57_split_encrypted_password_backup.js
@@ -1,10 +1,12 @@
 //2023-10-10_13-04-57_split_encrypted_password_backup
 
 exports.up = async function (db) {
-  await db.query('ALTER TABLE user_devices ADD COLUMN encrypted_password_backup_2 TEXT');
+  await db.query(
+    'ALTER TABLE user_devices ADD COLUMN IF NOT EXISTS encrypted_password_backup_2 TEXT',
+  );
   await db.query('UPDATE user_devices SET encrypted_password_backup_2=encrypted_password_backup');
 };
 
 exports.down = function (db) {
-  return db.query('ALTER TABLE user_devices DROP COLUMN encrypted_password_backup_2');
+  return db.query('ALTER TABLE user_devices DROP COLUMN IF EXISTS encrypted_password_backup_2');
 };
