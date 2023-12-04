13687d59fa74507e200524c8e9990457b4f5355e
Gireg <giregk@upsignon.eu>
2023-12-04 11:37:44 +0100

    add logs on old authenticate route

--- src/api1/routes/authenticate.ts
+++ src/api1/routes/authenticate.ts
@@ -1,6 +1,6 @@
 import { db } from '../../helpers/db';
 import { checkDeviceChallengeV1 } from '../helpers/deviceChallengev1';
-import { logError } from '../../helpers/logger';
+import { logError, logInfo } from '../../helpers/logger';
 import { checkPasswordChallengeV1 } from '../helpers/passwordChallengev1';
 import { inputSanitizer } from '../../helpers/sanitizer';
 import { SessionStore } from '../../helpers/sessionStore';
@@ -9,6 +9,7 @@ import { checkDeviceChallengeV2 } from '../../api2/helpers/deviceChallengev2';
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 export const authenticate = async (req: any, res: any) => {
   try {
+    logInfo(req.body?.userEmail, 'authenticateV5');
     const deviceUId = inputSanitizer.getString(req.body?.deviceId);
     const passwordChallengeResponse = inputSanitizer.getString(req.body?.passwordChallengeResponse);
     const deviceChallengeResponse = inputSanitizer.getString(req.body?.deviceChallengeResponse);
@@ -16,6 +17,7 @@ export const authenticate = async (req: any, res: any) => {
     const groupId = inputSanitizer.getNumber(req.params.groupId, 1);
 
     if (!userEmail || !deviceUId || !passwordChallengeResponse || !deviceChallengeResponse) {
+      logInfo(req.body?.userEmail, 'authenticateV5 KO: missing body param');
       return res.status(401).end();
     }
 
@@ -28,6 +30,7 @@ export const authenticate = async (req: any, res: any) => {
         ud.password_challenge_error_count AS password_challenge_error_count,
         char_length(ud.access_code_hash) > 0 AS has_access_code_hash,
         ud.device_public_key AS device_public_key,
+        ud.device_public_key_2 AS device_public_key_2,
         ud.session_auth_challenge AS session_auth_challenge,
         ud.session_auth_challenge_exp_time AS session_auth_challenge_exp_time
       FROM user_devices AS ud
@@ -41,11 +44,15 @@ export const authenticate = async (req: any, res: any) => {
       [userEmail, deviceUId, groupId],
     );
 
-    if (!dbRes || dbRes.rowCount === 0) return res.status(401).end();
+    if (!dbRes || dbRes.rowCount === 0) {
+      logInfo(req.body?.userEmail, 'authenticateV5 KO: not found');
+      return res.status(401).end();
+    }
     const {
       did,
       has_access_code_hash,
       device_public_key,
+      device_public_key_2,
       password_challenge_blocked_until,
       session_auth_challenge_exp_time,
       password_challenge_error_count,
@@ -55,13 +62,17 @@ export const authenticate = async (req: any, res: any) => {
     } = dbRes.rows[0];
 
     // 1 - check device uses the new cryptographic authentication mechanism
-    if (!!has_access_code_hash || !device_public_key) return res.status(401).end();
+    if (!!has_access_code_hash && !device_public_key && !device_public_key_2) {
+      logInfo(req.body?.userEmail, 'authenticateV5 KO: uses v4 auth');
+      return res.status(401).end();
+    }
 
     // 2 - check that the user is not temporarily blocked
     if (
       password_challenge_blocked_until &&
       password_challenge_blocked_until.getTime() > Date.now()
     ) {
+      logInfo(req.body?.userEmail, 'authenticateV5 KO: temporaryly blocked');
       return res.status(401).json({
         error: 'blocked',
         nextRetryDate: password_challenge_blocked_until.toISOString(),
@@ -73,6 +84,7 @@ export const authenticate = async (req: any, res: any) => {
       !session_auth_challenge ||
       session_auth_challenge_exp_time.getTime() < Date.now()
     ) {
+      logInfo(req.body?.userEmail, 'authenticateV5 KO: token expired');
       return res.status(403).json({ error: 'expired' });
     }
 
@@ -85,21 +97,29 @@ export const authenticate = async (req: any, res: any) => {
       did,
       groupId,
     );
-
+    if (!hasPassedPasswordChallenge) {
+      logInfo(req.body?.userEmail, 'authenticateV5 KO: wrong password');
+    }
     // 5 - check Device challenge
     let hasPassedDeviceChallenge = false;
-    if (device_public_key.length > 50) {
+    if (!!device_public_key) {
+      logInfo(req.body?.userEmail, 'authenticateV5 - checking device challenge v1');
       hasPassedDeviceChallenge = await checkDeviceChallengeV1(
         session_auth_challenge,
         deviceChallengeResponse,
         device_public_key,
       );
-    } else {
+    }
+    if (!hasPassedDeviceChallenge && !!device_public_key_2) {
+      logInfo(req.body?.userEmail, 'authenticateV5 - checking device challenge v2');
       hasPassedDeviceChallenge = await checkDeviceChallengeV2(
         session_auth_challenge,
         deviceChallengeResponse,
-        device_public_key,
+        device_public_key_2,
       );
+    }
+    if (!hasPassedDeviceChallenge) {
+      logInfo(req.body?.userEmail, 'authenticateV5 KO: device auth failed');
     }
 
     const success = hasPassedPasswordChallenge && hasPassedDeviceChallenge;
