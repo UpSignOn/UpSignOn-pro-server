09ed80935f95122429eb564d58847e9e216095fa
Gireg <giregk@upsignon.eu>
2023-12-13 16:54:17 +0100

    improve migration script

--- migrations/2023-12-13_14-12-16_move_device_last_session_date.js
+++ migrations/2023-12-13_14-12-16_move_device_last_session_date.js
@@ -1,9 +1,11 @@
 //2023-12-13_14-12-16_move_device_last_session_date
 
 exports.up = function (db) {
-  return db.query('ALTER TABLE user_devices ADD COLUMN last_sync_date TIMESTAMP WITH TIME ZONE');
+  return db.query(
+    'ALTER TABLE user_devices ADD COLUMN IF NOT EXISTS last_sync_date TIMESTAMP WITH TIME ZONE',
+  );
 };
 
 exports.down = function (db) {
-  return db.query('ALTER TABLE user_devices DROP COLUMN last_sync_date');
+  return db.query('ALTER TABLE user_devices DROP COLUMN IF EXISTS last_sync_date');
 };
