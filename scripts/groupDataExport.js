8552d039284ee71a42485cef8408083aca398ede
Gireg <giregk@upsignon.eu>
2024-01-26 09:41:59 +0100

    update export / import database group scripts

--- scripts/groupDataExport.js
+++ scripts/groupDataExport.js
@@ -20,6 +20,16 @@ const fs = require('fs');
 async function exportDb() {
   try {
     await db.connect();
+    const admins = await db.query('SELECT * FROM admins');
+    const admin_groups = await db.query('SELECT * FROM admin_groups WHERE group_id=$1', [groupId]);
+    const allowed_emails = await db.query('SELECT * FROM allowed_emails WHERE group_id=$1', [
+      groupId,
+    ]);
+    const data_stats = await db.query('SELECT * FROM data_stats WHERE group_id=$1', [groupId]);
+    // const password_reset_request = await db.query(
+    //   'SELECT * FROM password_reset_request WHERE group_id=$1',
+    //   [groupId],
+    // );
     const users = await db.query('SELECT * FROM users WHERE group_id=$1', [groupId]);
     const shared_accounts = await db.query('SELECT * FROM shared_accounts WHERE group_id=$1', [
       groupId,
@@ -31,15 +41,31 @@ async function exportDb() {
     const shared_folders = await db.query('SELECT * FROM shared_folders WHERE group_id=$1', [
       groupId,
     ]);
+    const shared_vault_recipients = await db.query(
+      'SELECT * FROM shared_vault_recipients WHERE group_id=$1',
+      [groupId],
+    );
+    const shared_vaults = await db.query('SELECT * FROM shared_vaults WHERE group_id=$1', [
+      groupId,
+    ]);
     const url_list = await db.query('SELECT * FROM url_list WHERE group_id=$1', [groupId]);
+    // const user_devices = await db.query('SELECT * FROM user_devices WHERE group_id=$1', [groupId]);
     fs.writeFileSync(
       filePath,
       JSON.stringify({
+        admins: admins.rows,
+        admin_groups: admin_groups.rows,
+        allowed_emails: allowed_emails.rows,
+        data_stats: data_stats.rows,
+        // password_reset_request: password_reset_request.rows,
+        shared_vault_recipients: shared_vault_recipients.rows,
+        shared_vaults: shared_vaults.rows,
+        // user_devices: user_devices.rows,
         users: users.rows,
-        sharedAccounts: shared_accounts.rows,
-        sharedAccountUsers: shared_account_users.rows,
-        sharedFolders: shared_folders.rows,
-        urlList: url_list.rows,
+        shared_accounts: shared_accounts.rows,
+        shared_account_users: shared_account_users.rows,
+        shared_folders: shared_folders.rows,
+        url_list: url_list.rows,
       }),
     );
     await db.release();
--- scripts/groupDataImport.js
+++ scripts/groupDataImport.js
@@ -22,14 +22,85 @@ const data = JSON.parse(dataString);
 async function importFunction() {
   await db.connect();
 
+  // ADMINS
+  for (var i = 0; i < data.admins.length; i++) {
+    const row = data.admins[i];
+    await db.query(
+      'INSERT INTO admins (id, email, password_hash, created_at) VALUES ($1,$2,$3,$4) ON CONFLICT DO NOTHING',
+      [row.id, row.email, row.password_hash, row.created_at],
+    );
+  }
+
+  // ADMIN GROUPS
+  for (var i = 0; i < data.admin_groups.length; i++) {
+    const row = data.admin_groups[i];
+    await db.query('INSERT INTO admin_groups (admin_id, group_id) VALUES ($1,$2)', [
+      row.admin_id,
+      groupId,
+    ]);
+  }
+
+  // ALLOWED EMAILS
+  for (var i = 0; i < data.allowed_emails.length; i++) {
+    const row = data.allowed_emails[i];
+    await db.query('INSERT INTO allowed_emails (pattern, group_id) VALUES ($1,$2)', [
+      row.pattern,
+      groupId,
+    ]);
+  }
+
+  // USERS
   for (var i = 0; i < data.users.length; i++) {
     const u = data.users[i];
     const insertedUser = await db.query(
-      'INSERT INTO users (email, encrypted_data, created_at, updated_at, sharing_public_key, group_id) VALUES ($1,$2,$3,$4,$5,$6) RETURNING id',
-      [u.email, u.encrypted_data, u.created_at, u.updated_at, u.sharing_public_key, groupId],
+      `INSERT INTO users (
+        email,
+        encrypted_data,
+        created_at,
+        updated_at,
+        sharing_public_key,
+        group_id,
+        nb_accounts,
+        nb_codes,
+        nb_accounts_strong,
+        nb_accounts_medium,
+        nb_accounts_weak,
+        nb_accounts_with_duplicated_password,
+        nb_accounts_with_no_password,
+        nb_accounts_red,
+        nb_accounts_orange,
+        nb_accounts_green,
+        encrypted_data_2,
+        sharing_public_key_2,
+        allowed_to_export,
+        allowed_offline_mobile,
+        allowed_offline_desktop) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21) RETURNING id`,
+      [
+        u.email,
+        u.encrypted_data,
+        u.created_at,
+        u.updated_at,
+        u.sharing_public_key,
+        groupId,
+        u.nb_accounts,
+        u.nb_codes,
+        u.nb_accounts_strong,
+        u.nb_accounts_medium,
+        u.nb_accounts_weak,
+        u.nb_accounts_with_duplicated_password,
+        u.nb_accounts_with_no_password,
+        u.nb_accounts_red,
+        u.nb_accounts_orange,
+        u.nb_accounts_green,
+        u.encrypted_data_2,
+        u.sharing_public_key_2,
+        u.allowed_to_export,
+        u.allowed_offline_mobile,
+        u.allowed_offline_desktop,
+      ],
     );
     const newId = insertedUser.rows[0].id;
-    data.sharedAccountUsers = data.sharedAccountUsers.map((sau) => {
+    data.shared_account_users = data.shared_account_users.map((sau) => {
       if (sau.user_id === u.id) {
         return {
           ...sau,
@@ -39,16 +110,37 @@ async function importFunction() {
         return sau;
       }
     });
+    data.data_stats = data.data_stats.map((row) => {
+      if (row.user_id === u.id) {
+        return {
+          ...row,
+          newUserId: newId,
+        };
+      } else {
+        return row;
+      }
+    });
+    data.shared_vault_recipients = data.shared_vault_recipients.map((row) => {
+      if (row.user_id === u.id) {
+        return {
+          ...row,
+          newUserId: newId,
+        };
+      } else {
+        return row;
+      }
+    });
   }
 
-  for (var i = 0; i < data.sharedFolders.length; i++) {
-    const sf = data.sharedFolders[i];
+  // SHARED FOLDERS
+  for (var i = 0; i < data.shared_folders.length; i++) {
+    const sf = data.shared_folders[i];
     const insertedSharedFolder = await db.query(
       'INSERT INTO shared_folders (name, group_id) VALUES ($1,$2) RETURNING id',
       [sf.name, groupId],
     );
     const newId = insertedSharedFolder.rows[0].id;
-    data.sharedAccounts = data.sharedAccounts.map((sa) => {
+    data.shared_accounts = data.shared_accounts.map((sa) => {
       if (sa.shared_folder_id === sf.id) {
         return {
           ...sa,
@@ -60,10 +152,11 @@ async function importFunction() {
     });
   }
 
-  for (var i = 0; i < data.sharedAccounts.length; i++) {
-    const sa = data.sharedAccounts[i];
+  // SHARED ACCOUNTS
+  for (var i = 0; i < data.shared_accounts.length; i++) {
+    const sa = data.shared_accounts[i];
     const insertedAcc = await db.query(
-      'INSERT INTO shared_accounts (url, name, login, type, created_at, aes_encrypted_data, shared_folder_id, group_id) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING id',
+      'INSERT INTO shared_accounts (url, name, login, type, created_at, aes_encrypted_data, shared_folder_id, group_id, is_migrated) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING id',
       [
         sa.url,
         sa.name,
@@ -73,10 +166,11 @@ async function importFunction() {
         sa.aes_encrypted_data,
         sa.newSharedFolderId,
         groupId,
+        sa.is_migrated,
       ],
     );
     const newId = insertedAcc.rows[0].id;
-    data.sharedAccountUsers = data.sharedAccountUsers.map((sau) => {
+    data.shared_account_users = data.shared_account_users.map((sau) => {
       if (sau.shared_account_id === sa.id) {
         return {
           ...sau,
@@ -88,8 +182,9 @@ async function importFunction() {
     });
   }
 
-  for (var i = 0; i < data.sharedAccountUsers.length; i++) {
-    const sau = data.sharedAccountUsers[i];
+  // SHARED_ACCOUNT_USERS
+  for (var i = 0; i < data.shared_account_users.length; i++) {
+    const sau = data.shared_account_users[i];
     await db.query(
       'INSERT INTO shared_account_users (shared_account_id, user_id, is_manager, created_at, encrypted_aes_key, group_id) VALUES ($1,$2,$3,$4,$5,$6)',
       [
@@ -103,11 +198,127 @@ async function importFunction() {
     );
   }
 
-  for (var i = 0; i < data.urlList.length; i++) {
-    const url = data.urlList[i];
+  // URL LIST
+  for (var i = 0; i < data.url_list.length; i++) {
+    const url = data.url_list[i];
     await db.query(
-      'INSERT INTO url_list (displayed_name, signin_url, password_change_url, group_id) VALUES ($1,$2,$3,$4)',
-      [url.displayed_name, url.signin_url, url.password_change_url, groupId],
+      'INSERT INTO url_list (displayed_name, signin_url, group_id, uses_basic_auth) VALUES ($1,$2,$3,$4)',
+      [url.displayed_name, url.signin_url, groupId, url.uses_basic_auth],
+    );
+  }
+
+  // SHARED VAULTS
+  for (var i = 0; i < data.shared_vaults.length; i++) {
+    const sv = data.shared_vaults[i];
+    const insertedVault = await db.query(
+      `INSERT INTO shared_vaults (
+        group_id,
+        name,
+        encrypted_data,
+        last_updated_at,
+        created_at,
+        nb_accounts,
+        nb_codes,
+        nb_accounts_strong,
+        nb_accounts_with_duplicated_password,
+        nb_accounts_with_no_password,
+        nb_accounts_red,
+        nb_accounts_orange,
+        nb_accounts_green,
+        content_details
+      ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) RETURNING id`,
+      [
+        groupId,
+        sv.name,
+        sv.encrypted_data,
+        sv.last_updated_at,
+        sv.created_at,
+        sv.nb_accounts,
+        sv.nb_codes,
+        sv.nb_accounts_strong,
+        sv.nb_accounts_with_duplicated_password,
+        sv.nb_accounts_with_no_password,
+        sv.nb_accounts_red,
+        sv.nb_accounts_orange,
+        sv.nb_accounts_green,
+        sv.content_details,
+      ],
+    );
+    const newId = insertedVault.rows[0].id;
+    data.shared_vault_recipients = data.shared_vault_recipients.map((svr) => {
+      if (svr.shared_vault_id === sv.id) {
+        return {
+          ...svr,
+          newSharedVaultId: newId,
+        };
+      } else {
+        return svr;
+      }
+    });
+    data.data_stats = data.data_stats.map((ds) => {
+      if (ds.shared_vault_id === sv.id) {
+        return {
+          ...ds,
+          newSharedVaultId: newId,
+        };
+      } else {
+        return ds;
+      }
+    });
+  }
+
+  // SHARED VAULT RECIPIENTS
+  for (var i = 0; i < data.shared_vault_recipients.length; i++) {
+    const svr = data.shared_vault_recipients[i];
+    await db.query(
+      'INSERT INTO shared_vault_recipients (shared_vault_id, user_id, encrypted_shared_vault_key, is_manager, group_id, created_at) VALUES ($1,$2,$3,$4,$5,$6)',
+      [
+        svr.newSharedVaultId,
+        svr.newUserId,
+        svr.encrypted_shared_vault_key,
+        svr.is_manager,
+        groupId,
+        svr.created_at,
+      ],
+    );
+  }
+
+  // DATA STATS
+  for (var i = 0; i < data.data_stats.length; i++) {
+    const row = data.data_stats[i];
+    await db.query(
+      `INSERT INTO data_stats (
+        user_id,
+        date,
+        nb_accounts,
+        nb_codes,
+        nb_accounts_strong,
+        nb_accounts_medium,
+        nb_accounts_weak,
+        nb_accounts_with_duplicated_password,
+        nb_accounts_with_no_password,
+        nb_accounts_red,
+        nb_accounts_orange,
+        nb_accounts_green,
+        group_id,
+        shared_vault_id
+      ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)`,
+      [
+        row.newUserId,
+        row.date,
+        row.nb_accounts,
+        row.nb_codes,
+        row.nb_accounts_strong,
+        row.nb_accounts_medium,
+        row.nb_accounts_weak,
+        row.nb_accounts_with_duplicated_password,
+        row.nb_accounts_with_no_password,
+        row.nb_accounts_red,
+        row.nb_accounts_orange,
+        row.nb_accounts_green,
+        groupId,
+        row.newSharedVaultId,
+      ],
     );
   }
 
