2cd6907b9fe13b9703fb36c2293aed97a67ed26a
Gireg <giregk@upsignon.eu>
2024-02-20 15:04:28 +0100

    add if exists to sql migration

--- migrations/2024-01-26_13-25-24_drop_delete_cascade_data_stats_for_user.js
+++ migrations/2024-01-26_13-25-24_drop_delete_cascade_data_stats_for_user.js
@@ -2,12 +2,12 @@
 
 exports.up = function (db) {
   return db.query(
-    'ALTER TABLE data_stats DROP CONSTRAINT stat_user_id, ADD CONSTRAINT stat_user_id FOREIGN KEY (user_id) REFERENCES users(id)',
+    'ALTER TABLE data_stats DROP CONSTRAINT IF EXISTS stat_user_id, ADD CONSTRAINT stat_user_id FOREIGN KEY (user_id) REFERENCES users(id)',
   );
 };
 
 exports.down = function (db) {
   return db.query(
-    'ALTER TABLE data_stats DROP CONSTRAINT stat_user_id, ADD CONSTRAINT stat_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE',
+    'ALTER TABLE data_stats DROP CONSTRAINT IF EXISTS stat_user_id, ADD CONSTRAINT stat_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE',
   );
 };
