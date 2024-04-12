3ed3a9aaf859a9f9954b489ded310ad4dd78f0e7
Gireg <giregk@upsignon.eu>
2024-04-12 11:44:37 +0200

    improve deployment scripts

--- post-update.sh
+++ post-update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ~/.npm-global/bin/yarn install
 ~/.npm-global/bin/yarn build
-~/.npm-global/bin/pm2 stop upsignon-pro-server
-~/.npm-global/bin/pm2 start ecosystem.config.js --update-env
+~/.npm-global/bin/pm2 startOrGracefulReload ecosystem.config.js --update-env
