4fa88a29ecca7865b0fcb0b7095a8c52caffda08
Gireg <giregk@upsignon.eu>
2024-07-01 11:06:13 +0200

    send admin notifications to superadmin only if there is no group admin

--- src/helpers/getAdminsEmailsForGroup.ts
+++ src/helpers/getAdminsEmailsForGroup.ts
@@ -1,10 +1,16 @@
 import { db } from './db';
 
 export const getAdminEmailsForGroup = async (groupId: number): Promise<string[]> => {
-  const admins = await db.query(
+  const groupAdmins = await db.query(
     'SELECT email FROM admins AS a LEFT JOIN admin_groups AS ag ON ag.admin_id = a.id WHERE a.is_superadmin OR ag.group_id=$1',
     [groupId],
   );
-  const adminEmails = admins.rows.map((admin) => admin.email);
-  return adminEmails;
+  if (groupAdmins.rowCount != null && groupAdmins.rowCount > 0) {
+    const adminEmails = groupAdmins.rows.map((admin) => admin.email);
+    return adminEmails;
+  } else {
+    const superAdmins = await db.query('SELECT email FROM admins WHERE a.is_superadmin', []);
+    const adminEmails = superAdmins.rows.map((admin) => admin.email);
+    return adminEmails;
+  }
 };
