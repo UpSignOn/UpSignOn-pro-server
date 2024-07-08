a3934438da602f21a40163dad55aeaeda3c8f968
Gireg <giregk@upsignon.eu>
2024-07-08 09:42:33 +0200

    fix admin emails

--- src/helpers/getAdminsEmailsForGroup.ts
+++ src/helpers/getAdminsEmailsForGroup.ts
@@ -2,7 +2,7 @@ import { db } from './db';
 
 export const getAdminEmailsForGroup = async (groupId: number): Promise<string[]> => {
   const groupAdmins = await db.query(
-    'SELECT email FROM admins AS a LEFT JOIN admin_groups AS ag ON ag.admin_id = a.id WHERE a.is_superadmin OR ag.group_id=$1',
+    'SELECT email FROM admins AS a INNER JOIN admin_groups AS ag ON ag.admin_id = a.id WHERE NOT a.is_superadmin AND ag.group_id=$1',
     [groupId],
   );
   if (groupAdmins.rowCount != null && groupAdmins.rowCount > 0) {
