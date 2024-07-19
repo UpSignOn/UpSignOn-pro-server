60d9739013e7c8bdba3ddf6604e0c4f9d5d85490
Gireg <giregk@upsignon.eu>
2024-07-19 13:55:06 +0200

    add hard backup before v5 deletion

--- migrations/2024-07-17_09-34-42_drop-v5.js
+++ migrations/2024-07-17_09-34-42_drop-v5.js
@@ -1,6 +1,28 @@
 //2024-07-17_09-34-42_drop-v5
+const { exec } = require('child_process');
 
 exports.up = async function (db) {
+  await new Promise((resolve, reject) => {
+    const dbName = process.env.DB_NAME;
+    exec(
+      `/usr/bin/pg_dump ${dbName} > ~/backup_before_v5_deletion.sql`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`error: ${error.message}`);
+          reject(error);
+          return;
+        }
+
+        if (stderr) {
+          console.error(`stderr: ${stderr}`);
+          reject(stderr);
+          return;
+        }
+
+        resolve();
+      },
+    );
+  });
   // DELETE vaults that have not migrated
   await db.query('DELETE FROM users WHERE encrypted_data_2 IS NULL');
   await db.query('DROP TABLE shared_account_users');
