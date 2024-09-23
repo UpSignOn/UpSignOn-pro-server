88df2486835240ab12bd34b4f15eab3ffeee1e38
Gireg <giregk@upsignon.eu>
2024-09-23 12:31:36 +0200

    add group setting to prevent update popup in ap

--- src/api2/routes/bank/getBankConfig.ts
+++ src/api2/routes/bank/getBankConfig.ts
@@ -8,7 +8,9 @@ export const getBankConfig = async (req: any, res: any): Promise<void> => {
     // TODO do not use a number ?
     const groupId = inputSanitizer.getNumber(req.params.groupId, 1);
 
-    const groupRes = await db.query('SELECT name, redirect_url FROM groups WHERE id=$1', [groupId]);
+    const groupRes = await db.query('SELECT name, redirect_url, settings FROM groups WHERE id=$1', [
+      groupId,
+    ]);
     if (groupRes.rowCount === 0) {
       logInfo(req.body?.userEmail, 'getBankConfig fail: bad group');
       return res.status(400).end();
@@ -17,6 +19,7 @@ export const getBankConfig = async (req: any, res: any): Promise<void> => {
     return res.status(200).json({
       newUrl: groupRes.rows[0].redirect_url,
       bankName: groupRes.rows[0].name,
+      preventUpdatePopup: groupRes.rows[0].settings.PREVENT_UPDATE_POPUP || false,
     });
   } catch (e) {
     logError('getBankConfig', e);
