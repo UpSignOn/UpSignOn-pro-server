266b70c0af0373564cdb5d9175cd8f80d3d165b1
Gireg <giregk@upsignon.eu>
2024-11-04 12:38:48 +0100

    fix db errors

---
+++ migrations/2024-11-04_12-33-18_fix_authentication_date.js
@@ -0,0 +1,18 @@
+//2024-11-04_12-33-18_fix_authentication_date
+
+exports.up = async function (db) {
+  await db.query(
+    'ALTER TABLE user_devices ALTER session_auth_challenge_exp_time TYPE timestamp with time zone;',
+  );
+  await db.query(
+    'ALTER TABLE user_devices ALTER password_challenge_blocked_until TYPE timestamp with time zone;',
+  );
+  await db.query(
+    'ALTER TABLE device_sessions ALTER expiration_time TYPE timestamp with time zone;',
+  );
+  await db.query(
+    'ALTER TABLE temporary_admins ALTER expiration_time TYPE timestamp with time zone;',
+  );
+};
+
+exports.down = function (db) {};
