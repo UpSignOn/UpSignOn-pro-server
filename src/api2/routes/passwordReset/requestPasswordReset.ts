a92f1e8d74fcac6af196081ae9b333e91725b357
Gireg <giregk@upsignon.eu>
2024-11-12 10:20:22 +0100

    when restarting a pwd reset request, also erase granted_by value

--- src/api2/routes/passwordReset/requestPasswordReset.ts
+++ src/api2/routes/passwordReset/requestPasswordReset.ts
@@ -146,7 +146,7 @@ export const requestPasswordReset2 = async (req: any, res: any) => {
       ) {
         // Start a new request
         await db.query(
-          `UPDATE password_reset_request SET status='PENDING_ADMIN_CHECK', reset_token=null, reset_token_expiration_date=null WHERE id=$1 AND group_id=$2`,
+          `UPDATE password_reset_request SET status='PENDING_ADMIN_CHECK', granted_by=null, reset_token=null, reset_token_expiration_date=null WHERE id=$1 AND group_id=$2`,
           [resetRequest.reset_request_id, groupId],
         );
         logInfo(req.body?.userEmail, 'requestPasswordReset2 OK (reset request updated)');
