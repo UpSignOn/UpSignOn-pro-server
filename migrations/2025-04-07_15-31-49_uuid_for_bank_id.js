b591f9fa73fe67130862060f6bfda3538cbc9426
Gireg <giregk@upsignon.eu>
2025-04-11 11:49:40 +0200

    fix gen_random_uuid() not existing for some clients

--- migrations/2025-04-07_15-31-49_uuid_for_bank_id.js
+++ migrations/2025-04-07_15-31-49_uuid_for_bank_id.js
@@ -1,6 +1,7 @@
 //2025-04-07_15-31-49_uuid_for_bank_id
 
 exports.up = async function (db) {
+  await db.query('CREATE EXTENSION pgcrypto IF NOT EXISTS');
   await db.query(
     'ALTER TABLE groups ADD COLUMN IF NOT EXISTS public_id UUID NOT NULL DEFAULT gen_random_uuid() UNIQUE',
   );
