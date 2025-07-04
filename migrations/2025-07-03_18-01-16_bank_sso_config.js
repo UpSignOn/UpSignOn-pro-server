798bb67a918ac455db48a7a75eaa6789a506fe98
Gireg <giregk@upsignon.eu>
2025-07-04 09:26:10 +0200

    create bank_sso_config

---
+++ migrations/2025-07-03_18-01-16_bank_sso_config.js
@@ -0,0 +1,11 @@
+//2025-07-03_18-01-16_bank_sso_config
+
+exports.up = function (db) {
+  return db.query(
+    'CREATE TABLE IF NOT EXISTS bank_sso_config (id SERIAL PRIMARY KEY, bank_id INT, openid_configuration_url VARCHAR, client_id VARCHAR, CONSTRAINT fk_bank_id FOREIGN KEY(bank_id) REFERENCES groups(id))',
+  );
+};
+
+exports.down = function (db) {
+  return db.query('DROP TABLE IF EXISTS bank_sso_config');
+};
