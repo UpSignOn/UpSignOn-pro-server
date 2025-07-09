eb6170c4d6452f4958c686980d7488ac2e8f00e4
Gireg <giregk@upsignon.eu>
2025-07-09 18:02:43 +0200

    add route authenticateWithAuthCode

--- src/helpers/sessionStore.ts
+++ src/helpers/sessionStore.ts
@@ -9,6 +9,11 @@ type SessionData = {
   deviceUniqueId: string;
   groupId: number;
 };
+type OpenIdSessionData = {
+  userEmail: string;
+  groupId: number;
+  accessToken: string;
+};
 
 async function cleanSessions() {
   try {
@@ -45,6 +50,27 @@ async function createSession(sessionData: SessionData): Promise<string> {
   );
   return getSignedSession(newSessionId);
 }
+async function createOpenIdSession(
+  sessionData: OpenIdSessionData,
+  expirationTimestamp: number | null,
+): Promise<string> {
+  const newSessionId = uuidv4();
+  const sessionDataString = JSON.stringify(sessionData);
+
+  let expDate;
+  if (expirationTimestamp) {
+    expDate = new Date(expirationTimestamp * 1000);
+  } else {
+    expDate = new Date();
+    expDate.setTime(expDate.getTime() + 3600 * 1000);
+  }
+  const expirationIso = expDate.toISOString().replace('T', ' ').replace('Z', '');
+  await db.query(
+    `INSERT INTO device_sessions (session_id, session_data, expiration_time) VALUES ($1, $2, $3)`,
+    [newSessionId, sessionDataString, expirationIso],
+  );
+  return getSignedSession(newSessionId);
+}
 
 async function checkSession(
   untrustedSession: string,
@@ -68,6 +94,28 @@ async function checkSession(
     expectedSessionData.groupId === actualSessionData.groupId
   );
 }
+async function checkOpenIdSession(
+  untrustedSession: string,
+  actualSessionData: OpenIdSessionData,
+): Promise<boolean> {
+  if (typeof untrustedSession !== 'string') return false;
+  const sessionId = untrustedSession.split('.')[0];
+  if (untrustedSession !== getSignedSession(sessionId)) return false;
+  // nb do use postgres time manipulation instead of js time to avoid issues with server time
+  const res = await db.query(
+    'SELECT session_data FROM device_sessions WHERE session_id = $1::text AND current_timestamp(0) <= expiration_time',
+    [sessionId],
+  );
+  if (res.rowCount === 0) {
+    return false;
+  }
+  const expectedSessionData = res.rows[0].session_data;
+  return (
+    expectedSessionData.userEmail === actualSessionData.userEmail &&
+    expectedSessionData.accessToken === actualSessionData.accessToken &&
+    expectedSessionData.groupId === actualSessionData.groupId
+  );
+}
 
 async function disconnectSession(untrustedSession: string) {
   if (typeof untrustedSession !== 'string') return null;
@@ -79,6 +127,8 @@ async function disconnectSession(untrustedSession: string) {
 export const SessionStore = {
   init,
   createSession,
+  createOpenIdSession,
   checkSession,
+  checkOpenIdSession,
   disconnectSession,
 };
