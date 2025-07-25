80dfada121521960a8994d4d4e00b9501eb6a1b9
Gireg <giregk@upsignon.eu>
2025-07-25 18:36:43 +0200

    add not null constraint

--- migrations/2025-07-18_17-03-52_read_only_superadmin.js
+++ migrations/2025-07-18_17-03-52_read_only_superadmin.js
@@ -1,7 +1,9 @@
 //2025-07-18_17-03-52_read_only_superadmin
 
 exports.up = function (db) {
-  return db.query('ALTER TABLE admins ADD COLUMN is_read_only_superadmin BOOLEAN DEFAULT false');
+  return db.query(
+    'ALTER TABLE admins ADD COLUMN is_read_only_superadmin BOOLEAN NOT NULL DEFAULT false',
+  );
 };
 
 exports.down = function (db) {
