b3d45e37b30bdbde2bfffb836888c82c50dcf5ec
Gireg <giregk@upsignon.eu>
2025-07-30 15:50:52 +0200

    11024 - part1 - rename local variables and text

--- src/api2/helpers/bankUUID.ts
+++ src/api2/helpers/bankUUID.ts
@@ -8,25 +8,25 @@ export type BankIds = {
 
 export class BadBankIdException extends Error {}
 
-/// This function transforms uuidv4 group id into integer group id
+/// This function transforms uuidv4 bank id into integer bank id
 /// and maintains backwards compatibility with previous id format
 export const getBankIds = async (req: any): Promise<BankIds> => {
   const rawId = req.params.bankUUID;
   let internalId, publicId;
   let usesDeprecatedIntId = false;
   if (typeof rawId === 'undefined' || rawId == null || rawId === 'undefined') {
-    // this is the deprecated default integer group id 1
+    // this is the deprecated default integer bank id 1
     internalId = 1;
   } else if (rawId.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)) {
     publicId = rawId;
   } else if (rawId.match(/^[1-9][0-9]{0,3}$/)) {
     internalId = Number.parseInt(rawId);
   } else {
-    throw new BadBankIdException('Bad groupId in req.params: ' + rawId);
+    throw new BadBankIdException('Bad bankId in req.params: ' + rawId);
   }
 
   if (internalId != null) {
-    // NB : usage of internal group ids for new banks is deprecated since 2025-04-08.
+    // NB : usage of internal bank ids for new banks is deprecated since 2025-04-08.
     // Let's allow this only for banks created before that date.
     const gRes = await db.query(
       "SELECT public_id FROM banks WHERE id=$1 AND created_at <= '2025-04-08'",
