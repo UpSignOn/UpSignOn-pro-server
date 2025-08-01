3460c45a0b92646383afeb79f11f42ae7368b2ea
Gireg <giregk@upsignon.eu>
2025-08-01 10:05:27 +0200

    11541 fix migration

--- migrations/2025-07-30_15-22-53_rename-groups-to-banks.js
+++ migrations/2025-07-30_15-22-53_rename-groups-to-banks.js
@@ -10,9 +10,6 @@ exports.up = async function (db) {
   await db.query('ALTER TABLE event_logs RENAME COLUMN group_id TO bank_id');
   await db.query('ALTER TABLE password_reset_request RENAME COLUMN group_id TO bank_id');
   await db.query('ALTER TABLE pwd_stats_evolution RENAME COLUMN group_id TO bank_id');
-  await db.query('ALTER TABLE shared_account_users RENAME COLUMN group_id TO bank_id');
-  await db.query('ALTER TABLE shared_accounts RENAME COLUMN group_id TO bank_id');
-  await db.query('ALTER TABLE shared_folders RENAME COLUMN group_id TO bank_id');
   await db.query('ALTER TABLE shared_vault_recipients RENAME COLUMN group_id TO bank_id');
   await db.query('ALTER TABLE shared_vaults RENAME COLUMN group_id TO bank_id');
   await db.query('ALTER TABLE url_list RENAME COLUMN group_id TO bank_id');
@@ -27,9 +24,6 @@ exports.down = async function (db) {
   await db.query('ALTER TABLE event_logs RENAME COLUMN bank_id TO group_id');
   await db.query('ALTER TABLE password_reset_request RENAME COLUMN bank_id TO group_id');
   await db.query('ALTER TABLE pwd_stats_evolution RENAME COLUMN bank_id TO group_id');
-  await db.query('ALTER TABLE shared_account_users RENAME COLUMN bank_id TO group_id');
-  await db.query('ALTER TABLE shared_accounts RENAME COLUMN bank_id TO group_id');
-  await db.query('ALTER TABLE shared_folders RENAME COLUMN bank_id TO group_id');
   await db.query('ALTER TABLE shared_vault_recipients RENAME COLUMN bank_id TO group_id');
   await db.query('ALTER TABLE shared_vaults RENAME COLUMN bank_id TO group_id');
   await db.query('ALTER TABLE url_list RENAME COLUMN bank_id TO group_id');
